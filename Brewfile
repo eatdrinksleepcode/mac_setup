72b360c1cc436763a9d972a69ed32da29c2c258b
David Nelson <eatdrinksleepcode@gmail.com>
2019-07-02 15:26:57 -0500

    Initial setup

---
+++ Brewfile
@@ -0,0 +1,18 @@
+tap "homebrew/cask"
+
+# Browsers
+cask "firefox"
+
+# Shell
+cask "iterm2"
+brew "zsh"
+brew "zsh-completions"
+brew "jq"
+brew "watch"
+brew "dos2unix"
+
+# General
+cask "disk-inventory-x"
+
+## Java
+cask "java"
---
+++ setUpMyMac.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+if [ $(which brew) ]; then
+  echo "Skipping installation of Homebrew (already installed)"
+else
+  ./homebrew-install.sh
+fi
+
+brew bundle
+
+if [ ! -e ~/.oh-my-zsh ]; then
+  ./oh-my-zsh-install.sh || exit
+else
+  echo "Skipping installation of Oh-My-Zsh (already installed)"
+fi
+if [ ! -e ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
+  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+else
+  echo "Skipping installation of PowerLevel9K theme (already installed)"
+fi
+#Set theme:
+#ZSH_THEME="powerlevel9k/powerlevel9k"
+if [ -e ~/Library/Fonts/"Droid Sans Mono for Powerline Nerd Font Complete.otf" ]; then
+  echo "Skipping installation of Powerline font (already installed)"
+else
+  curl -fLo ~/Library/Fonts/"Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.otf
+fi
+cp .zshrc ~/.zshrc
+cp -f com.googlecode.iterm2.plist ~/Library/Preferences
+
+cp .gitconfig ~/.gitconfig
+
