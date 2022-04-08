7730f449079194e025f60ed273d6b7ea06c92ab5
David Nelson <eatdrinksleepcode@gmail.com>
2022-04-08 14:37:20 -0500

    Ignore grep output

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -5,6 +5,6 @@ targetGroup=brew
 
 echo "Setting up '${targetUser}' to use Homebrew..."
 echo "Loooking for the 'brew' group:"
-(dscl . list /Groups | grep "^${targetGroup}$") || (sudo dseditgroup -o create "${targetGroup}" -r "Homebrew users" && echo "Group '${targetGroup}' created")
+(dscl . list /Groups | grep "^${targetGroup}$" > /dev/null) || (sudo dseditgroup -o create "${targetGroup}" -r "Homebrew users" && echo "Group '${targetGroup}' created")
 echo "Checking for membership in the '${targetGroup}' group:"
-(id -Gn "${targetUser}" | tr ' ' '\n' | grep "^${targetGroup}$") || (sudo dseditgroup -o edit -a "${targetUser}" -t user "${targetGroup}" && echo "User '${targetUser}' added to group '${targetGroup}'")
+(id -Gn "${targetUser}" | tr ' ' '\n' | grep "^${targetGroup}$" > /dev/null) || (sudo dseditgroup -o edit -a "${targetUser}" -t user "${targetGroup}" && echo "User '${targetUser}' added to group '${targetGroup}'")
