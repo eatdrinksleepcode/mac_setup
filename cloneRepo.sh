a380a2a32d144f7d4e732bb1109ea104e67c2942
David Nelson <eatdrinksleepcode@gmail.com>
2022-04-08 12:12:26 -0500

    Add script to clone and run the repo

---
+++ cloneRepo.sh
@@ -0,0 +1,6 @@
+# run this script by pasting this command into the terminal:
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/eatdrinksleepcode/mac_setup/master/cloneRepo.sh)"
+
+git clone https://github.com/eatdrinksleepcode/mac_setup.git
+cd mac_setup
+./setUpMyMac.sh
